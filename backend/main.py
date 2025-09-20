ca4697aeac6d9b45927b64d2b952bb37a14890af
Akshit Yadav <akshit07032001@gmail.com>
2025-09-20 17:13:25 +0530

    config setup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
 app = FastAPI(
     title="Choose Your Own Adventure GAme API", 
@@ -12,12 +13,15 @@ app = FastAPI(
 # Have our api's used from a different origin
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["*"], 
+    allow_origins=settings.ALLOWED_ORIGINS, 
     allow_credentials=True, 
     allow_methods=["*"], 
     allow_headers=["*"]
 )
 
+# coming to this statement, it tells only execute this statement if we are 
+# directly executing this file
 if __name__ == "__main__": 
+    # uvicorn: webserver, used to server our FastAPI application
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
