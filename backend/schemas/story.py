2adcb3ca67308bcf69cdf650cbc15efe229ac553
Akshit Yadav <akshit07032001@gmail.com>
2025-09-20 18:25:14 +0530

    schemas setup

---
+++ backend/schemas/story.py
@@ -0,0 +1,41 @@
+# defines the type of data to come into the api and the type of 
+# data that can go out from the api 
+# this is important to do some data validation by FastAPI
+from typing import List, Optional, Dict
+from datetime import datetime
+from pydantic import BaseModel
+
+class StoryOptionsSchema(BaseModel):
+    text:str 
+    node_id: Optional[int] = None
+
+class StoryNodeBase(BaseModel):
+    content:str 
+    is_ending:bool = False 
+    is_winning_ending: bool = False 
+
+class CompleteStoryNodeResponse(StoryNodeBase):
+    id: int 
+    options: List[StoryOptionsSchema] = []
+
+    class Config: 
+        from_attributes = True
+
+class StoryBase(BaseModel):
+    title:str 
+    session_id: Optional[str] = None
+
+    class Config: 
+        from_attributes = True
+
+class CreateStoryRequest(BaseModel):
+    theme:str 
+
+class CompleteStoryResponse(StoryBase):
+    id: int 
+    created_at: datetime
+    root_node: CompleteStoryNodeResponse
+    all_nodes: Dict[int, CompleteStoryNodeResponse]
+
+    class Config: 
+        from_attributes = True
