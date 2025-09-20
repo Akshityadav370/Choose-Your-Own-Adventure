a2a328ea9f12f78e3c34ed71ce92d17c98918864
Akshit Yadav <akshit07032001@gmail.com>
2025-09-20 22:24:34 +0530

    frontend setup

---
+++ frontend/src/components/LoadingStatus.jsx
@@ -0,0 +1,16 @@
+const LoadingStatus = ({ theme }) => {
+  return (
+    <div className='loading-container'>
+      <h2>Generate your {theme} Story</h2>
+
+      <div className='loading-animation'>
+        <div className='spinner'></div>
+      </div>
+      <p className='loading-info'>
+        Please wait while we generate your story...
+      </p>
+    </div>
+  );
+};
+
+export default LoadingStatus;
